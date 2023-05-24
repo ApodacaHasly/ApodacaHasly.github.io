4c41d9e4b47d64a5451e76126c6503cdbb0f1c39
ApodacaHasly <124411148+ApodacaHasly@users.noreply.github.com>
2023-05-23 18:11:48 -0600

    Practica1 terminada 23/05/2023

---
+++ parcial3/parcial3/Practica1.php
@@ -0,0 +1,22 @@
+<?php
+
+ echo "<h2>Hola Mundo</h2>";
+
+ echo "<hr>";
+
+  $nombre ="Hasly";
+  $edad =17;
+
+  if($edad >=17){
+    echo $nombre . " es mayor de edad";
+  }else{
+    echo " es menor de edad ";
+  }
+  echo "<br><br>";
+    for($i=0; $i<10; $i++){
+    echo $i. "<br>";
+  }
+
+
+  
+ ?>
