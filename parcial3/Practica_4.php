0cedf2fda6c0d9a1cfa535fbbe28bdaf5e66871e
ApodacaHasly <124411148+ApodacaHasly@users.noreply.github.com>
2023-06-02 18:44:52 -0700

    Practica 5 02/03/2023

--- parcial3/Practica_4.php
+++ parcial3/Practica_4.php
@@ -5,7 +5,7 @@ $calificacion = 0;
 
 $Pregunta1 = $_POST["Pregunta1" ];
 
- /*$Pregunta2 = $_POST["Pregunta2" ];
+ $Pregunta2 = $_POST["Pregunta2" ];
 $Pregunta3 = $_POST["Pregunta3" ];
 $Pregunta4 = $_POST["Pregunta4" ];
 $Pregunta5 = $_POST["Pregunta5" ];
@@ -14,7 +14,7 @@ $Pregunta7 = $_POST["Pregunta7" ];
 $Pregunta8 = $_POST["Pregunta8" ];
 $Pregunta9 = $_POST["Pregunta9" ];
 $Pregunta10 = $_POST["Pregunta10"];
-$Pregunta11 = $_POST["Pregunta11"];*/
+$Pregunta11 = $_POST["Pregunta11"];
 
 echo "<h2>Alumno: " .$nombre. "<h2>";
 
@@ -30,6 +30,77 @@ if($Pregunta1 == "b"){
     echo "<p> Respuesta es (b) </p>";
     echo "<img src='incorrecto.png' width='3%'><hr>";
 }
+if($Pregunta2 == "c"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (c) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta3 == "d"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (d) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta4 == "c"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (c) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta5 == "c"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (c) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta6 == "b"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (b) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta7 == "b"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (b) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta8 == "a"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (a) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta9 == "a"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (a) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta10 == "c"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (c) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+if($Pregunta11 == "c"){
+    $calificacion ++;
+    echo "<img src= 'correcta.png' width='3%>'";
+}else{
+    echo "<p> Respuesta es (c) </p>";
+    echo "<img src='incorrecto.png' width='3%'><hr>";
+}
+
 
 if($calificacion >= 0 && $calificacion <= 5){
     echo "<h3>Calificacion:" .$calificacion.", No aprobado (Se requiere estudiar)</h3>";
